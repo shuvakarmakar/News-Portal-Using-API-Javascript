c898b32855af7812dc3cafd6443182c5ea230cd5
shuvakarmakar <shuva15-7953@diu.edu.bd>
2023-03-02 18:25:47 +0600

    NavBar and Alert Section Dynamic Done

--- js/app.js
+++ js/app.js
@@ -13,7 +13,7 @@ const showCategories = data =>{
         let linkContainer = document.createElement('p');
         linkContainer.innerHTML = `
             <a class="nav-link" href="#" onclick="fetchCategoryNews
-            ('${singleCatagory.category_id}')">${singleCatagory?.category_name}</a>
+            ('${singleCatagory.category_id}', '${singleCatagory.category_name}')">${singleCatagory.category_name}</a>
     `;
         categoriesContainer.appendChild(linkContainer);
     })
@@ -21,11 +21,20 @@ const showCategories = data =>{
 
 }
 
-const fetchCategoryNews = (category_id) =>{
+const fetchCategoryNews = (category_id, category_name) =>{
         const url = `https://openapi.programming-hero.com/api/news/category/${category_id}`
         fetch(url)
         .then(res => res.json())
-        .then(data => console.log(data))
+        .then(data => showAllNews(data.data, category_name))
     }
+
+
+const showAllNews = (data, category_name) =>{
+    console.log(data, category_name)
+    document.getElementById('news-count').innerText = data.length;
+    document.getElementById('category-name').innerText = category_name;
+}
+
+
 
 fetchCategories();
