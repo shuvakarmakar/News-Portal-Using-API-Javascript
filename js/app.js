1c1cefc54dc3b5ca88950c2c8b83f8bcc5be77f6
shuvakarmakar <shuva15-7953@diu.edu.bd>
2023-03-02 20:38:30 +0600

    Modal Dynamic Done

--- js/app.js
+++ js/app.js
@@ -33,6 +33,71 @@ const showAllNews = (data, category_name) =>{
     console.log(data, category_name)
     document.getElementById('news-count').innerText = data.length;
     document.getElementById('category-name').innerText = category_name;
+
+    const newsContainer = document.getElementById('all-news');
+    newsContainer.innerHTML = '';
+
+    data.forEach(singleNews =>{
+        const{_id, image_url, title, details, author, total_view} = singleNews;
+
+        const card = document.createElement('div');
+        card.classList.add('card', 'mb-3')
+        card.innerHTML = `
+        <div class="card mb-3">
+        <div class="row g-0">
+          <div class="col-md-4">
+            <img src="${image_url}" class="img-fluid rounded-start" alt="...">
+          </div>
+          <div class="col-md-8 d-flex flex-column">
+            <div class="card-body">
+              <h5 class="card-title">${title}</h5>
+              <p class="card-text">
+              ${details.slice(0, 200)}...
+              </p>
+            </div>
+            <div class="card-footer border-0 bg-body d-flex justify-content-between">
+                <div class="col-md-4 d-flex gap-2">
+                    <img src="${author.img}" class="img-fluid rounded-circle" alt="..." height="40" width="40">
+                    <div>
+                    <p class="m-0 p-0">${author.name}</p>
+                    <p class="m-0 p-0">${author.published_date}</p>
+                    </div>
+                </div>
+
+                <div class="d-flex align-items-center">
+                    <i class= "fas fa-eye"></i>
+                    <p class="m-0 p-0">${total_view}</p>
+                </div>
+
+                <div>
+                    <i class= "fas fa-star"></i>
+                </div>
+
+                <div>
+                    <i class= "fas fa-arrow-right"></i>
+                    <p class="m-0 p-0" onclick="fetchNewsDetail('${_id}')" data-bs-toggle="modal" data-bs-target="#exampleModal">Click Here</p>
+                </div>
+                
+            </div>
+          </div>
+        </div>
+      </div>
+        `
+        newsContainer.appendChild(card);
+    });
+};
+
+
+const fetchNewsDetail = news_id =>{
+    let url = `https://openapi.programming-hero.com/api/news/${news_id}`
+    fetch(url)
+        .then(res => res.json())
+        .then(data => showNewDetail(data.data[0]))
+}
+
+
+const showNewDetail = newsDetail =>{
+    // 
 }
 
 
